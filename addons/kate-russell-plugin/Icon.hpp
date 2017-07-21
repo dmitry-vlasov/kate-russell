fe0f7ded480232792e85c84f18ea2d8f8a4336a3
Dmitry Vlasov <vlasov@academ.org>
2017-07-21 11:57:18 +0700

    Icon class made only header

--- addons/kate-russell-plugin/Icon.hpp
+++ addons/kate-russell-plugin/Icon.hpp
@@ -19,29 +19,95 @@ namespace russell {
 
 class Icon {
 public :  
-	static const QIcon& get (const Kind);
+	static const QIcon& get (const Kind kind) {
+		switch (kind) {
+		case SOURCE     : return source();
+		case IMPORT     : return import();
+		case THEORY     : return theory();
+		case CONTENTS   : return contents();
+		case CONSTANT   : return constant();
+		case TYPE       : return type();
+		case RULE       : return rule();
+		case AXIOM      : return axiom();
+		case DEFINITION : return definition();
+		case THEOREM    : return theorem();
+		case PROBLEM    : return problem();
+		case PROOF      : return proof();
+		default         : return theory();
+		}
+	}
 
 	// Russell source elements
-	static const QIcon& source();
-	static const QIcon& import();
-	static const QIcon& theory();
-	static const QIcon& contents();
-	static const QIcon& constant();
-	static const QIcon& type();
-	static const QIcon& rule();
-	static const QIcon& axiom();
-	static const QIcon& definition();
-	static const QIcon& theorem();
-	static const QIcon& problem();
-	static const QIcon& proof();
+	static const QIcon& source() {
+		static QIcon theory(QStringLiteral(":/katerussell/icons/hi16-actions-russell-theory.png"));
+		return theory;
+	}
+	static const QIcon& import() {
+		static QIcon import(QStringLiteral(":/katerussell/icons/hi16-actions-russell-import.png"));
+		return import;
+	}
+	static const QIcon& theory() {
+		static QIcon theory(QStringLiteral(":/katerussell/icons/hi16-actions-russell-theory.png"));
+		return theory;
+	}
+	static const QIcon& contents() {
+		static QIcon theory(QStringLiteral(":/katerussell/icons/hi16-actions-russell-theory.png"));
+		return theory;
+	}
+	static const QIcon& constant() {
+		static QIcon constant(QStringLiteral(":/katerussell/icons/hi16-actions-russell-constant.png"));
+		return constant;
+	}
+	static const QIcon& rule() {
+		static QIcon rule(QStringLiteral(":/katerussell/icons/hi16-actions-russell-rule.png"));
+		return rule;
+	}
+	static const QIcon& type() {
+		static QIcon type(QStringLiteral(":/katerussell/icons/hi16-actions-russell-type.png"));
+		return type;
+	}
+	static const QIcon& axiom() {
+		static QIcon axiom(QStringLiteral(":/katerussell/icons/hi16-actions-russell-axiom.png"));
+		return axiom;
+	}
+	static const QIcon& definition() {
+		static QIcon definition(QStringLiteral(":/katerussell/icons/hi16-actions-russell-definition.png"));
+		return definition;
+	}
+	static const QIcon& theorem() {
+		static QIcon theorem(QStringLiteral(":/katerussell/icons/hi16-actions-russell-theorem.png"));
+		return theorem;
+	}
+	static const QIcon& problem() {
+		static QIcon problem(QStringLiteral(":/katerussell/icons/hi16-actions-russell-problem.png"));
+		return problem;
+	}
+	static const QIcon& proof() {
+		static QIcon proof(QStringLiteral(":/katerussell/icons/hi16-actions-russell-proof.png"));
+		return proof;
+	}
 
 	// Proof tree elements
-	static const QIcon& root();
-	static const QIcon& hyp();
-	static const QIcon& prop();
-	static const QIcon& ref();
-	static const QIcon& top();
-	//static const QIcon& proved();
+	static const QIcon& root() {
+		static QIcon root(QStringLiteral(":/katerussell/icons/hi16-actions-russell-root.png"));
+		return root;
+	}
+	static const QIcon& hyp() {
+		static QIcon hyp(QStringLiteral(":/katerussell/icons/hi16-actions-russell-hyp.png"));
+		return hyp;
+	}
+	static const QIcon& prop() {
+		static QIcon prop(QStringLiteral(":/katerussell/icons/hi16-actions-russell-prop.png"));
+		return prop;
+	}
+	static const QIcon& ref() {
+		static QIcon ref(QStringLiteral(":/katerussell/icons/hi16-actions-russell-ref.png"));
+		return ref;
+	}
+	static const QIcon& top() {
+		static QIcon top(QStringLiteral(":/katerussell/icons/hi16-actions-russell-top.png"));
+		return top;
+	}
 };
 
 }
